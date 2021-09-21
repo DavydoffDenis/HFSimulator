e8ef14dfa8444c759f00a0b65098f0d7d87a146f
Denis Davydov <sir.davydoff@yandex.ru>
2021-09-21 09:07:41 +0300

    bugfix in Popen_logger 'a0' underrun added

--- Popen_logger.py
+++ Popen_logger.py
@@ -6,13 +6,14 @@ def au_debuginfo_func(err_line_stream):
     print(f"Проскальзывание: {datetime.datetime.now().time()}")
     while True:
         read_d_char = process.stderr.read(2)
-        if read_d_char == b'aU':
+        if read_d_char == b'aU' or read_d_char == b'aO':
             print(f"Проскальзывание: {datetime.datetime.now().time()}")
         if not read_char: return
         time.sleep(0.01)
 
 process = subprocess.Popen(["python3", "HFSimulator.py"], stderr=subprocess.PIPE)
-if process.stderr.read(2) == b'aU':
+read_d_char = process.stderr.read(2)
+if read_d_char == b'aU' or read_d_char == b'aO':
     au_debuginfo_func(process.stderr)
 while True:
     read_char = process.stderr.read(1)
@@ -20,7 +21,7 @@ while True:
     if read_char == b'\n':
         read_d_char = process.stderr.read(2)
         print(read_d_char)
-        if read_d_char == b'aU':
+        if read_d_char == b'aU' or read_d_char == b'aO':
             au_debuginfo_func(process.stderr)
     if not read_char: break
     time.sleep(0.01)
